2c989ce63157c34df21e1f43de5f7bf1db2db26d
Rogerio Maateus Ricardo <rogerio22ricardo@gmail.com>
2019-09-19 11:47:06 -0300

    Configurando Rotas da Pagina de Erro de Conexão e Manutenção

--- source/App/Web.php
+++ source/App/Web.php
@@ -4,6 +4,7 @@
 namespace Source\App;
 
 
+use Source\Core\Connect;
 use Source\Core\Controller;
 use Source\Support\Pager;
 
@@ -209,17 +210,38 @@ class Web extends Controller
     public function error(array $data): void
     {
         $error = new \stdClass();
-        $error->code = $data['errcode'];
-        $error->title = "Ooops. Conteúdo indisponível :/";
-        $error->message = "Sentimos muito, mas o conteúdo que você tentou acessar não eciste, está indisponível no momento ou foi removido :/";
-        $error->linkTitle = "Continue navegando!";
-        $error->link = url_back();
+
+        switch ($data['errcode']) {
+            case "problemas":
+                $error->code = "OPS";
+                $error->title = "Estamos enfrentando problemas!";
+                $error->message = "Parece que nosso serviço não está diponível no momento. Já estamos vendo isso mas caso precise, envie um e-mail :)";
+                $error->linkTitle = "ENVIAR E-MAIL";
+                $error->link = "mailto:" . CONF_MAIL_SUPPORT;
+                break;
+
+            case "manutencao":
+                $error->code = "OPS";
+                $error->title = "Desculpe. Estamos em manutenção!";
+                $error->message = "Voltamos logo! Por hora estamos trabalhando para melhorar nosso conteúdo para você controlar melhor as suas contas :P";
+                $error->linkTitle = null;
+                $error->link = null;
+                break;
+
+            default:
+                $error->code = $data['errcode'];
+                $error->title = "Ooops. Conteúdo indispinível :/";
+                $error->message = "Sentimos muito, mas o conteúdo que você tentou acessar não existe, está indisponível no momento ou foi removido :/";
+                $error->linkTitle = "Continue navegando!";
+                $error->link = url_back();
+                break;
+        }
 
         $head = $this->seo->render(
-            "{$error->code}" | "{$error->title}",
+            "{$error->code} | {$error->title}",
             $error->message,
-            url_back("/ops/{$error->code}"),
-            theme("/assets/images/share.jps"),
+            url("/ops/{$error->code}"),
+            theme("/assets/images/share.jpg"),
             false
         );
 
--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -90,6 +90,7 @@ define("CONF_MAIL_PORT", "587");
 define("CONF_MAIL_USER", "apikey");
 define("CONF_MAIL_PASS", "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");
 define("CONF_MAIL_SENDER", ["name" => "Rogerio Mateus Ricardo", "address" => "rogerio22ricardo@gmail.com"]);
+define("CONF_MAIL_SUPPORT", "cursos@upinside.com.br");
 define("CONF_MAIL_OPTION_LANG", "br");
 define("CONF_MAIL_OPTION_HTML", true);
 define("CONF_MAIL_OPTION_AUTH", true);
