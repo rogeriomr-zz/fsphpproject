56fed2c8ff118c5991d173a00c243c3e98899101
Rogerio Maateus Ricardo <rogerio22ricardo@gmail.com>
2019-09-25 11:36:40 -0300

    Implementando o Gráfico

--- source/App/App.php
+++ source/App/App.php
@@ -4,6 +4,7 @@ namespace Source\App;
 
 use Source\Core\Controller;
 use Source\Models\Auth;
+use Source\Models\CafeApp\AppInvoice;
 use Source\Models\Report\Access;
 use Source\Models\Report\Online;
 use Source\Models\User;
@@ -47,8 +48,49 @@ class App extends Controller
             false
         );
 
+        //CHART
+        $dateChart = [];
+        for ($month = -4; $month <= 0; $month++) {
+            $dateChart[] = date("m/Y", strtotime("{$month}month"));
+        }
+
+        $chartData = new \stdClass();
+        $chartData->categories = "'" . implode("','", $dateChart) . "'";
+        $chartData->expense = "0,0,0,0,0";
+        $chartData->income = "0,0,0,0,0";
+
+        $chart = (new AppInvoice())
+            ->find("user_id = :user AND status = :status AND due_at >= DATE(now() - INTERVAL 4 MONTH) GROUP BY year(due_at) ASC, month(due_at) ASC",
+                "user={$this->user->id}&status=paid",
+                "
+                    year(due_at) AS due_year,
+                    month(due_at) AS due_month,
+                    DATE_FORMAT(due_at, '%m/%Y') AS due_date,
+                    (SELECT SUM(value) FROM app_invoices WHERE user_id = :user AND status = :status AND type = 'income' AND year(due_at) = due_year AND month(due_at) = due_month) AS income,
+                    (SELECT SUM(value) FROM app_invoices WHERE user_id = :user AND status = :status AND type = 'expense' AND year(due_at) = due_year AND month(due_at) = due_month) AS expense
+                "
+            )->limit(5)->fetch(true);
+
+        if ($chart) {
+            $chartCategories = [];
+            $chartExpense = [];
+            $chartIncome = [];
+
+            foreach ($chart as $chartItem) {
+                $chartCategories[] = $chartItem->due_date;
+                $chartExpense[] = $chartItem->expense;
+                $chartIncome[] = $chartItem->income;
+            }
+
+            $chartData->categories = "'" . implode("','", $chartCategories) . "'";
+            $chartData->expense = implode(",", array_map("abs", $chartExpense));
+            $chartData->income = implode(",", array_map("abs", $chartIncome));
+        }
+        //END CHART
+
         echo $this->view->render("home", [
-            "head" => $head
+            "head" => $head,
+            "chart" => $chartData
         ]);
     }
 
--- source/Models/CafeApp/AppInvoice.php
+++ source/Models/CafeApp/AppInvoice.php
@@ -8,8 +8,10 @@ class AppInvoice extends Model
 {
     public function __construct()
     {
-        parent::__construct("app_invoice", ["id"],
-            ["user_id", "wallet_id", "category_id", "description", "type", "value", "due_at", "repeat_when"]);
+        parent::__construct(
+            "app_invoices", ["id"],
+            ["user_id", "wallet_id", "category_id", "description", "type", "value", "due_at", "repeat_when"]
+        );
     }
 }
 
