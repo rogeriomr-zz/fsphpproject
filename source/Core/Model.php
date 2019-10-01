c7a57b4bea14fb4d1225cf67b8f4a0ee19de8334
Rogerio Maateus Ricardo <rogerio22ricardo@gmail.com>
2019-10-01 11:37:15 -0300

    Plano, pedido e pagamento

--- source/Core/Model.php
+++ source/Core/Model.php
@@ -31,13 +31,13 @@ abstract class Model
     protected $offset;
 
     /** @var string $entity database table */
-    protected static $entity;
+    protected $entity;
 
     /** @var array $protected no update or create */
-    protected static $protected;
+    protected $protected;
 
     /** @var array $entity database table */
-    protected static $required;
+    protected $required;
 
     /**
      * Model constructor.
@@ -47,9 +47,9 @@ abstract class Model
      */
     public function __construct(string $entity, array $protected, array $required)
     {
-        self::$entity = $entity;
-        self::$protected = array_merge($protected, ['created_at', "updated_at"]);
-        self::$required = $required;
+        $this->entity = $entity;
+        $this->protected = array_merge($protected, ['created_at', "updated_at"]);
+        $this->required = $required;
 
         $this->message = new Message();
     }
@@ -118,12 +118,12 @@ abstract class Model
     public function find(?string $terms = null, ?string $params = null, string $columns = "*")
     {
         if ($terms) {
-            $this->query = "SELECT {$columns} FROM " . static::$entity . " WHERE {$terms}";
+            $this->query = "SELECT {$columns} FROM {$this->entity} WHERE {$terms}";
             parse_str($params, $this->params);
             return $this;
         }
 
-        $this->query = "SELECT {$columns} FROM " . static::$entity;
+        $this->query = "SELECT {$columns} FROM {$this->entity}";
         return $this;
     }
 
@@ -214,7 +214,7 @@ abstract class Model
             $columns = implode(", ", array_keys($data));
             $values = ":" . implode(", :", array_keys($data));
 
-            $stmt = Connect::getInstance()->prepare("INSERT INTO " . static::$entity . " ({$columns}) VALUES ({$values})");
+            $stmt = Connect::getInstance()->prepare("INSERT INTO {$this->entity} ({$columns}) VALUES ({$values})");
             $stmt->execute($this->filter($data));
 
             return Connect::getInstance()->lastInsertId();
@@ -240,7 +240,7 @@ abstract class Model
             $dateSet = implode(", ", $dateSet);
             parse_str($params, $params);
 
-            $stmt = Connect::getInstance()->prepare("UPDATE " . static::$entity . " SET {$dateSet} WHERE {$terms}");
+            $stmt = Connect::getInstance()->prepare("UPDATE {$this->entity} SET {$dateSet} WHERE {$terms}");
             $stmt->execute($this->filter(array_merge($data, $params)));
             return ($stmt->rowCount() ?? 1);
         } catch (\PDOException $exception) {
@@ -290,7 +290,7 @@ abstract class Model
     public function delete(string $terms, ?string $params): bool
     {
         try {
-            $stmt = Connect::getInstance()->prepare("DELETE FROM " . static::$entity . " WHERE {$terms}");
+            $stmt = Connect::getInstance()->prepare("DELETE FROM {$this->entity} WHERE {$terms}");
             if ($params) {
                 parse_str($params, $params);
                 $stmt->execute($params);
@@ -324,7 +324,7 @@ abstract class Model
     protected function safe(): ?array
     {
         $safe = (array)$this->data;
-        foreach (static::$protected as $unset) {
+        foreach ($this->protected as $unset) {
             unset($safe[$unset]);
         }
         return $safe;
@@ -349,7 +349,7 @@ abstract class Model
     protected function required(): bool
     {
         $data = (array)$this->data();
-        foreach (static::$required as $field) {
+        foreach ($this->required as $field) {
             if (empty($data[$field])) {
                 return false;
             }
--- source/Models/CafeApp/AppSubscription.php
+++ source/Models/CafeApp/AppSubscription.php
@@ -2,12 +2,47 @@
 namespace Source\Models\CafeApp;
 
 use Source\Core\Model;
+use Source\Models\User;
 
 class AppSubscription extends Model
 {
     public function __construct()
     {
         parent::__construct("app_subscription", ["id"], ["user_id", "plan_id", "card_id", "status", "pay_status", "started", "due_day", "next_due"]);
+    }
+
+    /**
+     * @param User $user
+     * @param AppPlan $plan
+     * @param AppCreditCard $card
+     * @return AppSubscription
+     * @throws \Exception
+     */
+    public function subscribe(User $user, AppPlan $plan, AppCreditCard $card): AppSubscription
+    {
+        $this->user_id = $user->id;
+        $this->plan_id = $plan->id;
+        $this->card_id = $card->id;
+        $this->status = "active";
+        $this->pay_status = "active";
+        $this->started = date("Y-m-d");
+
+        $day = (new \DateTime($this->started))->format("d");
+
+        if ($day <= 28) {
+            $this->due_day = $day;
+            $this->next_due = date("Y-m-d", strtotime("+{$plan->period}"));
+        } else {
+            $due_day = 5;
+            $next_due = date("Y-m-{$due_day}", strtotime("+{$plan->period}"));
+
+            $this->due_day = $due_day;
+            $this->next_due = date("Y-m-d", strtotime($next_due . "+1month"));
+        }
+
+        $this->last_charge = date("Y-m-d");
+        $this->save();
+        return $this;
     }
 
     /**
